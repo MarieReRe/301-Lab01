42a4343cc5461604d11ae72e510f099828bb4844
Marie Antons <maantons@mail.fhsu.edu>
2020-04-01 21:45:01 -0500

    added color randomizer with jQuery

--- app.js
+++ app.js
@@ -1,49 +1,74 @@
 'use strict'
 
 
-$(document).ready(function () {
-    let back = ["#ff0000", "blue", "#ffd700", "#00ff7f", "#ff6347"];
-    let rand = back[Math.floor(Math.random() * back.length)];
-    $('header').css('background', rand);
-   
+$(window).resize(function () {
+    if ($(this).width() > 400) {
+        let back = ["#ff0000", "blue", "#ffd700", "#00ff7f", "#ff6347"];
+        let rand = back[Math.floor(Math.random() * back.length)];
+        $('header').css('background', rand);
+    }
 })
-$(document).ready(function () {
-    let back = ["#7fffd4", "#ffb6c1", "#ff00ff", "#008080", "#90ee90"];
-    let rand = back[Math.floor(Math.random() * back.length)];
-    $('footer').css('background', rand);
+$(window).resize(function () {
+    if ($(this).width() > 400) {
+        let back = ["#7fffd4", "#ffb6c1", "#ff00ff", "#008080", "#90ee90"];
+        let rand = back[Math.floor(Math.random() * back.length)];
+        $('footer').css('background', rand);
+    }
 })
-$(document).ready(function () {
-    let back = ["#a8ddd1", "#9370db", "#c71585", "#6b8e23", "#2e8b57"];
-    let rand = back[Math.floor(Math.random() * back.length)];
-    $('aside').css('background', rand);
+$(window).resize(function () {
+    if ($(this).width() > 400) {
+        let back = ["#a8ddd1", "#9370db", "#c71585", "#6b8e23", "#2e8b57"];
+        let rand = back[Math.floor(Math.random() * back.length)];
+        $('aside').css('background', rand);
+    }
+
 })
-$(document).ready(function () {
-    let back = ["#ff8c00", "#dc143c", "#7fffd4", "#ffe4c4", "#800080"];
-    let rand = back[Math.floor(Math.random() * back.length)];
-    $('.boxC').css('background', rand);
+$(window).resize(function () {
+    if ($(this).width() > 400) {
+        let back = ["#ff8c00", "#dc143c", "#7fffd4", "#ffe4c4", "#800080"];
+        let rand = back[Math.floor(Math.random() * back.length)];
+        $('.boxC').css('background', rand);
+    }
+
 })
-$(document).ready(function () {
-    let back = ["#ba55d3", "#9370db", "#c71585", "#6b8e23", "#003d3f"];
-    let rand = back[Math.floor(Math.random() * back.length)];
-    $('.boxD').css('background', rand);
+$(window).resize(function () {
+    if ($(this).width() > 400) {
+        let back = ["#ba55d3", "#9370db", "#FF0099", "#6b8e23", "#003d3f"];
+        let rand = back[Math.floor(Math.random() * back.length)];
+        $('.boxD').css('background', rand);
+    }
+
 })
-$(document).ready(function () {
-    let back = ["#f2e562", "#9370db", "#c71585", "#6b8e23", "#e6e5ec"];
-    let rand = back[Math.floor(Math.random() * back.length)];
-    $('.boxE').css('background', rand);
+$(window).resize(function () {
+    if ($(this).width() > 400) {
+        let back = ["#f2e562", "#9370db", "	#FF0000", "#FF6600", "#e6e5ec"];
+        let rand = back[Math.floor(Math.random() * back.length)];
+        $('.boxE').css('background', rand);
+    }
+
 })
-$(document).ready(function () {
-    let back = ["#582d91", "#480a9a", "#3b0051", "#522e91", "#8900ff"];
-    let rand = back[Math.floor(Math.random() * back.length)];
-    $('.boxF').css('background', rand);
+$(window).resize(function () {
+    if ($(this).width() > 400) {
+        let back = ["#582d91", "#480a9a", "#3b0051", "#522e91", "#8900ff"];
+        let rand = back[Math.floor(Math.random() * back.length)];
+        $('.boxF').css('background', rand);
+    }
+
 })
-$(document).ready(function () {
-    let back = ["#ba55d3", "#9370db", "#c71585", "#6b8e23", "#b25c66"];
-    let rand = back[Math.floor(Math.random() * back.length)];
-    $('.boxG').css('background', rand);
+$(window).resize(function () {
+    if ($(this).width() > 400) {
+        let back = ["#ba55d3", "#9370db", "#c71585", "#6b8e23", "#b25c66"];
+        let rand = back[Math.floor(Math.random() * back.length)];
+        $('.boxG').css('background', rand);
+    }
+
 })
-$(document).ready(function () {
-    let back = ["#ba55d3", "#9370db", "#c71585", "#6b8e23", "#2e8b57"];
-    let rand = back[Math.floor(Math.random() * back.length)];
-    $('.boxH').css('background', rand);
+$(window).resize(function () {
+    if ($(this).width() > 400) {
+        let back = ["#ba55d3", "#9370db", "#c71585", "#6b8e23", "#2e8b57"];
+        let rand = back[Math.floor(Math.random() * back.length)];
+        $('.boxH').css('background', rand);
+    }
+
 })
+
